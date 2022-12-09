fc238548750faa7a124f1206fb75ae96e98692d8
Radium <2043869032@qq.com>
2022-12-10 00:07:19 +0800

    chore: commit

--- src/core/Canvas2DApplication.ts
+++ src/core/Canvas2DApplication.ts
@@ -11,3 +11,15 @@ export class Canvas2DApplication extends Application {
     this.context2D = this.canvas.getContext('2d', contextAttributes);
   }
 }
+
+export class WebGLApplication extends Application {
+  public context3D: WebGLRenderingContext | null;
+
+  public constructor(
+    canvas: HTMLCanvasElement,
+    contextAttributes?: WebGLContextAttributes,
+  ) {
+    super(canvas);
+    this.context3D = this.canvas.getContext('webgl', contextAttributes);
+  }
+}
